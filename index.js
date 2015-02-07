2ba7ceabcfa170813b2c38a469e5c77cff2fdd20
Berto Yáñez <bertez@gmail.com>
2015-02-08 00:08:31 +0100

    Added db

--- index.js
+++ index.js
@@ -3,15 +3,47 @@ var fs = require('fs');
 var Twit = require('twit');
 
 var config = fs.existsSync('./local.config.js') ? require('./local.config.js') : require('./config.js');
-
 var T = new Twit(config.oauth);
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var TweetsSchema = new Schema({
+	_id: ObjectId,
+	tweet: {}
+});
+
+mongoose.model("TweetsSchema", TweetsSchema);
+
+var tweetsModel = mongoose.model("TweetsSchema", "tweets");
+
+mongoose.connect("mongodb://localhost/queremos");
 
 console.log('Init process');
 console.log(config);
 
+function addTweet(tweet) {
+	var t = new tweetsModel();
+	t.tweet = tweet;
+
+	t.tweet.created_at = new Date(tweet.created_at);
+
+	t.save(function(err) {
+		if (err) {
+			console.log(err)
+			return;
+		};
+
+		console.log("Added to db:", tweet.id);
+	});
+
+};
+
 var stream = T.stream('statuses/filter', { track: config.track });
 
 stream.on('tweet', function (tweet) {
+	addTweet(tweet);
   console.log('GOT TWEET', tweet.text);
 });
 
